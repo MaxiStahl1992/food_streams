b85f7501cc47ecc47f2dd56c4674346a26774a38
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2022-06-26 14:54:25 +0200

    Main page: Complete Header, Start Slider

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:food_streams/home/food_page_body.dart';
+import 'package:food_streams/style/colors.dart';
+import 'package:food_streams/widgets/big_text.dart';
+import 'package:food_streams/widgets/small_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Germany", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: "München", color: Colors.black54,),
+                          const Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                      child: const Icon(Icons.search, color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
